606be769ac1769e445b1d056be4f59456e31b3e0
sugiryota <83449662+sugiryota@users.noreply.github.com>
2021-06-09 19:56:41 +0900

    issue1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,31 +1,30 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index,:show]
-  before_action :set_item, only: [:show,:edit,:update,:destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def new
-    @item=Item.new
+    @item = Item.new
   end
 
   def create
-    @item=Item.new(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to root_path
     else
       render :new
-    end  
+    end
   end
 
   def index
-    @items=Item.all.order('created_at DESC')
+    @items = Item.all.order('created_at DESC')
   end
 
   def show
-
   end
 
   def edit
-
   end
+
   def update
     if @item.update(item_params)
       redirect_to root_path
@@ -33,6 +32,7 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
   def destroy
     @item.destroy
     redirect_to root_path
@@ -41,7 +41,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name,:text,:audio,:category_id,:url,:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :text, :audio, :category_id, :url, :image).merge(user_id: current_user.id)
   end
 
   def set_item
--- app/models/item.rb
+++ app/models/item.rb
@@ -9,12 +9,9 @@ class Item < ApplicationRecord
     validates :name
     validates :category_id
     validates :audio
-
   end
 
-  validates :category_id,numericality: { other_than: 1 }
+  validates :category_id, numericality: { other_than: 1 }
 
   mount_uploader :audio, AudioUploader
-
-  
 end
